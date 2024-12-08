be1aa21de46d033bd75e77f96857d67f8836eb1e
Tarandeep Singh <tarun.mt62@gmail.com>
2024-12-08 17:10:35 +0530

    understanding fragments

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./scm2.0/src/main/resources/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode:"selector",
+};
+
