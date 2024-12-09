3a49acd915737d03c837cf932ec3ed303ccc56cd
Tarandeep Singh <tarun.mt62@gmail.com>
2024-12-09 18:50:39 +0530

    trn commit 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./scm2.0/src/main/resources/**/*.{html,js}"],
+  content: ["./src/main/resources/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode:"selector",
+  darkMode: "selector",
 };
-
